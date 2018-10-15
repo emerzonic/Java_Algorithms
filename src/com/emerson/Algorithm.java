e4a81c1b1c84e054dfbd926452896a962b8fec4e
Emerson Doyah <emerson.doyah@gmail.com>
2018-10-15 10:00:48 -0500

    refactor

--- src/com/emerson/Algorithm.java
+++ src/com/emerson/Algorithm.java
@@ -7,14 +7,12 @@ public class Algorithm {
     //Find the first dupulicate in a list. return -1 if no duplicate
     public static int firstDuplicate(int[] a) {
         ArrayList<Integer> tracker = new ArrayList<>();
-        int nextOccurance = -1;
         for(int number: a){
             if(tracker.contains(number)){
-                nextOccurance = number;
-                return nextOccurance;
+                return number;
             }
             tracker.add(number);
         }
-        return nextOccurance;
+        return -1;
     }
 }
